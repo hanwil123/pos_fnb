e1534b1d7a745261c00526e9a5e1eb57894befb5
Farhan Wildan <farhanwildan12366@gmail.com>
2026-09-17 13:55:25 +0700

    fix: update ngrok API URL in fetch request

--- app/[qr_token]/page.tsx
+++ app/[qr_token]/page.tsx
@@ -9,7 +9,7 @@ export default async function Page({
 }) {
   const { qr_token } = await params
 
-const res = await fetch(`https://6ca2-180-251-183-213.ngrok-free.app/api/v1/table/${qr_token}`, {
+const res = await fetch(`https://a3eb-180-251-183-213.ngrok-free.app/api/v1/table/${qr_token}`, {
   cache: 'no-store',
   headers: {
     'ngrok-skip-browser-warning': 'true',
